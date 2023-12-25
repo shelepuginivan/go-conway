14b743d2847b0c780802018895277498a927a453
shelepuginivan <shelepuginivanm@gmail.com>
2023-12-25 06:06:29 +0300

    test(conway): add test for methods `GetCell` and `SetCell`

--- pkg/conway/conway_test.go
+++ pkg/conway/conway_test.go
@@ -5,6 +5,71 @@ import (
 	"testing"
 )
 
+func TestConwayGetCell(t *testing.T) {
+	game := New([][]bool{
+		{false, false, false, false, false},
+		{false, false, true, false, false},
+		{false, false, true, false, false},
+		{false, false, true, false, false},
+		{false, false, false, false, false},
+	})
+
+	cases := []struct {
+		x        int
+		y        int
+		expected bool
+	}{
+		{x: 0, y: 0, expected: false},
+		{x: 4, y: 1, expected: false},
+		{x: 2, y: 2, expected: true},
+		{x: 3, y: 2, expected: false},
+		{x: 2, y: 3, expected: true},
+	}
+
+	for _, c := range cases {
+		actual := game.GetCell(c.x, c.y)
+
+		if actual != c.expected {
+			t.Errorf("Expected cell to be %t, but got %t", c.expected, actual)
+		}
+	}
+}
+
+func TestConwaySetCell(t *testing.T) {
+	game := New([][]bool{
+		{false, false, false, false, false},
+		{false, false, true, false, false},
+		{false, false, true, false, false},
+		{false, false, true, false, false},
+		{false, false, false, false, false},
+	})
+
+	cases := []struct {
+		x int
+		y int
+		v bool
+	}{
+		{x: 0, y: 0, v: true},
+		{x: 4, y: 1, v: false},
+		{x: 4, y: 4, v: false},
+		{x: 3, y: 1, v: false},
+		{x: 2, y: 4, v: false},
+		{x: 4, y: 0, v: false},
+		{x: 3, y: 2, v: true},
+		{x: 2, y: 3, v: false},
+	}
+
+	for _, c := range cases {
+		game.SetCell(c.x, c.y, c.v)
+		newValue := game.GetCell(c.x, c.y)
+
+		if newValue != c.v {
+			t.Errorf("Expected cell to be %t, but got %t", c.v, newValue)
+		}
+	}
+
+}
+
 func TestConwayTick(t *testing.T) {
 	initialGrid := [][]bool{
 		{false, false, false, false, false},
